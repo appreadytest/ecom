a3f7c547e142ca61a2d097fbbba4d80d7ab6db6c
Mike Mike <mikemike@Mikes-MacBook-Pro.local>
2022-06-20 09:59:33 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => 
+      import('./product/product.module').then(
+        a => a.ProductModule
+      )
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
